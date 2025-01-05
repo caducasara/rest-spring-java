9688282ee5feeb45f680c0d037cae49bf9dfe182
caducasara <carloscasara98@gmail.com>
2025-01-05 16:34:10 -0300

    applying CORS to specific routes

---
+++ 14_CORS/rest-spring-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES 
+	(1,'Santa Cristina do Pinhal, 330, Parobé, 95630000','Carlos','Male','Casara'),
+	(2,'São Paulo','Ayrton','Male','Senna'),(5,'Texas - USA','Sheldon','Male','Cooper'),
+	(6,'USA','Sheldon','Male','Cooper'),(7,'Brazil','Teste','Female','v1');
